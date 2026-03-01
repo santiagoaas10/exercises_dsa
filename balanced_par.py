4611e5f8fe6c5286479d07e4b926f9ffee63eeee
Santiago Alvarez <santiagoaas10@gmail.com>
2026-03-01 09:47:22 -0500

    valid parentheses solution

--- balanced_par.py
+++ balanced_par.py
@@ -1,2 +1,19 @@
 def check_balanced_par(s):
-    pass
+    mappings = {
+        ')' : '(',
+        ']' : '[',
+        '}' : '{'
+    }
+    stack = []
+    for c in s:
+        if c in mappings.values():
+            stack.append(c)
+        elif c in mappings.keys() and len(stack) >0 :
+            if mappings[c] == stack[-1]:
+                stack.pop()
+            else: 
+                return False
+    return len(stack) == 0
+
+
+    
